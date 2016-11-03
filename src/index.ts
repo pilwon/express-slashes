60bda7e0197fd84e8950188ec20034067a5b4813
Pilwon Huh <pilwon@gmail.com>
2016-11-03 14:20:38 +0700

    Removed unnecessary backslash. Bumped to v0.1.1

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export = () => {
     const redirectUrl = req.url
       .replace(/(\/+)/g, '/')           // collapse repeated slashes
       .replace(/\/((?:\?.*)?)$/, '$1')  // remove trailing slash while preserving querystring
-      .replace(/^([^\/]|$)/, '/$1');    // ensure url starts with a slash
+      .replace(/^([^/]|$)/, '/$1');     // ensure url starts with a slash
     if (req.url !== redirectUrl && req.url !== '/') {
       res.redirect(redirectUrl);
       return;
